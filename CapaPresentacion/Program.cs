fcdfcf62ef2b5394907e99b5aebd1b751408c55f
darien005 <darien.briceno@outlook.com>
2025-10-28 23:26:34 -0600

    Pantalla PagoEmpleados

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmPagoEmpleados());
+            Application.Run(new Form1());
         }
     }
 }
